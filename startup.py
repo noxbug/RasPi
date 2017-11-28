14a59ce1c37fac7e976c4d3e3594ae028660a054
noxbug <spruitje1>
2017-11-28 21:04:17 +0100

    initial version

--- startup.py
+++ startup.py
@@ -1,14 +1,14 @@
 from subprocess import check_output, call
 from time import sleep
 
+
 def start_process(process):
     try:
         # find PID to check if process is already running
         pid = check_output(['pgrep', process]).decode('utf-8')
     except:
-        # start process if not running
         sleep(1)
+        # start process if not running
         call(process + ' &', shell=True)
 
-
-start_process('leafpad')
+start_process('kodi')
