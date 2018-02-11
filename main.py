1c7e378b2ad22e14b864688cd3f4ef89ee849966
noxbug <pieterjan.dingenen@outlook.com>
2018-02-11 16:15:32 +0100

    style status indicator

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from kodi import Kodi
 
 kodi = Kodi('192.168.1.10')
-help = kodi.help('Player.Stop')
-kodi.stop()
+# help = kodi.help('Player.Stop')
+# kodi.stop()
 position = kodi.fast_forward()
 player = kodi.get_active_players()
 status = kodi.play_pause()
