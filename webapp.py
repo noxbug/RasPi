f04a05ff25713ccb558d371a60167a0f5f998a68
noxbug <pieterjan.dingenen@outlook.com>
2018-02-11 15:31:45 +0100

    add base.html template

--- webapp.py
+++ webapp.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, redirect, url_for
 from kodi import Kodi
 
-webapp = Flask(__name__)
+app = Flask(__name__)
 
 kodi = Kodi('192.168.1.10')
 play = False
 
-@webapp.route('/')
+@app.route('/')
+@app.route('/index')
 def index():
     global kodi
     global play
@@ -24,15 +25,15 @@ def index():
         title = 'Nothing Playing'
         artist = 'Unkown'
         album_art_url = '/static/ben.jpg'
-    return render_template('index.html', title=title, artist=artist, album_art=album_art_url, play=play)
+    return render_template('now_playing.html', title=title, artist=artist, album_art=album_art_url, play=play)
 
-@webapp.route('/previous')
+@app.route('/previous')
 def previous():
     global kodi
     kodi.fast_rewind()
     return redirect(url_for('index'))
 
-@webapp.route('/play')
+@app.route('/play')
 def play():
     global kodi
     global play
@@ -42,35 +43,35 @@ def play():
         play = False
     return redirect(url_for('index'))
 
-@webapp.route('/stop')
+@app.route('/stop')
 def stop():
     global kodi
     kodi.stop()
     return redirect(url_for('index'))
 
-@webapp.route('/next')
+@app.route('/next')
 def next():
     global kodi
     kodi.fast_forward()
     return redirect(url_for('index'))
 
-@webapp.route('/translate_sub')
+@app.route('/translate_sub')
 def translate_sub():
     global kodi
     kodi.translate_subtitle()
     return redirect(url_for('index'))
 
-@webapp.route('/next_sub')
+@app.route('/next_sub')
 def next_sub():
     global kodi
     kodi.next_subtitle()
     return redirect(url_for('index'))
 
-@webapp.route('/update')
+@app.route('/update')
 def update():
     global kodi
     kodi.update_library()
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    webapp.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
