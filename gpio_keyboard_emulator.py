3b0d84d85d14e11bc694b46f60f2b09485fa9e70
noxbug <spruitje1>
2018-03-06 23:52:44 +0100

    initial version

--- gpio_keyboard_emulator.py
+++ gpio_keyboard_emulator.py
@@ -1,5 +1,8 @@
+import RPIO as GPIO
+import signal
 import time
 import json
+
 
 # load keymap configuration
 try:
@@ -7,17 +10,40 @@ try:
         keymap = json.load(fid)
 except:
     print('Oops something went wrong! Load default keymap configuration')
-    keymap = {1:{'keyboard': 'KEY_W', 'controller': 'UP'},
-                2:{'keyboard': 'KEY_S', 'controller': 'DOWN'},
-                3:{'keyboard': 'KEY_A', 'controller': 'LEFT'},
-                4:{'keyboard': 'KEY_D', 'controller': 'RIGHT'}}
+    keymap =    {6:{'controller': 'UP',     'keyboard': 'KEY_W'},
+                13:{'controller': 'DOWN',   'keyboard': 'KEY_S'},
+                19:{'controller': 'LEFT',   'keybaord': 'KEY_A'},
+                26:{'controller': 'RIGHT',  'keybaord': 'KEY_D'},
+                12:{'controller': 'A',      'keybaord': 'KEY_D'},
+                16:{'controller': 'B',      'keybaord': 'KEY_D'},
+                20:{'controller': 'X',      'keybaord': 'KEY_D'},
+                21:{'controller': 'Y',      'keybaord': 'KEY_D'},
+                23:{'controller': 'SELECT', 'keybaord': 'KEY_D'},
+                22:{'controller': 'START',  'keybaord': 'KEY_D'},
+                27:{'controller': 'L1',     'keybaord': 'KEY_D'},
+                17:{'controller': 'R1',     'keybaord': 'KEY_D'}}
 
-print('Done')
 
+# callback functions
+def gpio_callback(pin, value):
+    print(value)
+
+
+# register interrupt
+bouncetime = round(1/30*1000)  # 30 FPS
+for pin in keymap:
+    GPIO.add_interrupt_callback(int(pin), gpio_callback, edge='both', pull_up_down=GPIO.PUD_UP, threaded_callback=True, debounce_timeout_ms=bouncetime)
 
 # save keymap
 with open('keymap.json', 'w') as fid:
     json.dump(keymap, fid, indent=4, sort_keys=True, ensure_ascii=False)
 
+
+# main loop
+try:
+    print('Press CTRL+C to stop to exit')
+    signal.pause()
+except KeyboardInterrupt:
+    GPIO.cleanup()  # clean up
 
 print('all done')
