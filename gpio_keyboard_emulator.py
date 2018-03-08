e7b4f866d8f74c78b42e740e698762cfa009784c
pieterjan <spruitje1>
2018-03-08 20:25:57 +0100

    initial version

--- gpio_keyboard_emulator.py
+++ gpio_keyboard_emulator.py
@@ -6,12 +6,17 @@ import json
 import uinput
 
 # load uinput kernel module
-subprocess.call(['sudo', 'modprobe', 'uinput'])
+subprocess.call(['sudo', '-i', 'modprobe', 'uinput'])
 
-# start pgpio daemon
 try:
-	pid = subprocess.
-subprocess.call(['sudo', 'pigpiod'])
+    # check if pigpio daemon running
+    pid = subprocess.check_output(['pgrep', 'pigpiod']).encode("utf-8")
+    print(pid)
+except:
+    # start if needed
+    print('Starting pigpio daemon...')
+    subprocess.call(['sudo', 'pigpiod'])
+    time.sleep(1)
 
 # create pigpio instance
 gpio = pigpio.pi()
@@ -39,30 +44,43 @@ except:
 with open('keymap.json', 'w') as fid:
     json.dump(keymap, fid, indent=4, sort_keys=True, ensure_ascii=False)
 
-# callback functions
-def gpio_callback(pin, level, tick):
-    print(str(pin) + ' level: ' + str(level) + ' @ ' + str(tick))
-
-# setup gpio + events
-glitch_time = round(1/30*1000)  # 30 FPS
+# parse keymap for faster indexing
+keymap_str_index = keymap
+keymap = {}
 events = []
-for key in keymap:
-	# gpio
-	pin = int(key)
-	gpio.set_mode(pin, pigpio.INPUT)
-	gpio.set_pull_up_down(pin, pigpio.PUD_UP)
-	gpio.set_glitch_filter(pin, glitch_time)
-	gpio.callback(pin, pigpio.EITHER_EDGE, gpio_callback)
-	# events
-	keymap[key]['keyboard'] = eval('uinput.KEY_' + keymap[key]['keyboard'])
+for key in keymap_str_index:
+    int_key = int(key)
+    keymap[int_key] = keymap_str_index[key]
+    keymap[int_key]['keyboard'] = eval('uinput.KEY_' + keymap[int_key]['keyboard'])
+    events.append(keymap[int_key]['keyboard'])
+del(keymap_str_index)
 
-print(keymap)
+# create uinput device
+dev = uinput.Device(events)
 
 
+# callback function
+# 0: falling edge
+# 1: rising edge
+# 2: watchdog timeout
+level_conversion = {0: 1, 1: 0}
+def gpio_callback(pin, level, tick):
+    dev.emit(keymap[pin]['keyboard'], level_conversion[level])
+    # debug
+    # print(str(pin) + ' level: ' + str(level) + ' @ ' + str(tick))
+
+
+# setup gpio
+glitch_filter_time = round(1/30*1000)  # 30 FPS
+for pin in keymap:
+    gpio.set_mode(pin, pigpio.INPUT)
+    gpio.set_pull_up_down(pin, pigpio.PUD_UP)
+    gpio.set_glitch_filter(pin, glitch_filter_time)
+    gpio.callback(pin, pigpio.EITHER_EDGE, gpio_callback)
 
 # main loop
 try:
-    print('Press CTRL+C to exit')
+    print('Press Ctrl+C to exit')
     signal.pause()
 except KeyboardInterrupt:
     # clean up
