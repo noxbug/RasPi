798855cc941a64c77d0a31372fb4d2c39e44b060
pieterjan <spruitje1>
2018-06-18 15:17:15 +0200

    rm kodirpc library

--- lib/kodirpc/connection.py
+++
@@ -1,4 +0,0 @@
-# this file contains global variables shared between the modules
-# they act much like the attributes of a class (self.attribute)
-global host
-global port
--- lib/kodirpc/gui.py
+++
@@ -1,6 +0,0 @@
-from . import rpc as _rpc
-
-
-def up():
-    """Toggle fullscreen/GUI"""
-    _rpc.request('GUI.SetFullscreen')
